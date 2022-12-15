651aaeca8e231b9e700baefe61afd2136b37a386
a88dsz598 <casualattitude0@buho.game.tw>
2022-12-15 23:21:18 +0800

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 const backgoundImage = new Image();
 backgoundImage.onload = function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
   ctx.save();
   ctx.globalAlpha = 1;
   ctx.drawImage(backgoundImage, x, y, _width, _height);
@@ -11,116 +11,116 @@ backgoundImage.onload = function () {
 };
 backgoundImage.src = "./assets/hint/background0.webp";
 
-canvas.addEventListener("click", function () {
-  if (isplaying) return;
+// canvas.addEventListener("click", function () {
+//   if (isplaying) return;
 
-  isplaying = true;
-  window.requestAnimationFrame(updateAnimat1);
-});
+//   isplaying = true;
+//   window.requestAnimationFrame(updateAnimat1);
+// });
 
-function updateAnimteHint() {
-  if (isplaying) return;
+// function updateAnimteHint() {
+//   if (isplaying) return;
 
-  currentTime = new Date().getTime();
-  if (currentTime - lastTime < FRAME_PERIOD_IDLE) {
-    requestAnimationFrame(updateAnimteHint);
-    return;
-  }
+//   currentTime = new Date().getTime();
+//   if (currentTime - lastTime < FRAME_PERIOD_IDLE) {
+//     requestAnimationFrame(updateAnimteHint);
+//     return;
+//   }
 
-  hintIndex++;
-  if (hintIndex >= hintIndexMax) {
-    hintIndex = 0;
-  }
+//   hintIndex++;
+//   if (hintIndex >= hintIndexMax) {
+//     hintIndex = 0;
+//   }
 
-  backgoundImage.src = getIdelIndex(hintIndex);
+//   backgoundImage.src = getIdelIndex(hintIndex);
 
-  lastTime = currentTime;
-  window.requestAnimationFrame(updateAnimteHint);
-}
+//   lastTime = currentTime;
+//   window.requestAnimationFrame(updateAnimteHint);
+// }
 
-function updateAnimat1() {
-  currentTime = new Date().getTime();
-  if (currentTime - lastTime < FRAME_PERIOD) {
-    requestAnimationFrame(updateAnimat1);
-    return;
-  }
+// function updateAnimat1() {
+//   currentTime = new Date().getTime();
+//   if (currentTime - lastTime < FRAME_PERIOD) {
+//     requestAnimationFrame(updateAnimat1);
+//     return;
+//   }
 
-  frameIndex++;
-  if (frameIndex >= frameIndexMax) {
-    frameIndex = frameIndexMax;
-    updateAnimat2();
-    return;
-  }
+//   frameIndex++;
+//   if (frameIndex >= frameIndexMax) {
+//     frameIndex = frameIndexMax;
+//     updateAnimat2();
+//     return;
+//   }
 
-  backgoundImage.src = getFrameIndex(frameIndex);
+//   backgoundImage.src = getFrameIndex(frameIndex);
 
-  lastTime = currentTime;
-  window.requestAnimationFrame(updateAnimat1);
-}
+//   lastTime = currentTime;
+//   window.requestAnimationFrame(updateAnimat1);
+// }
 
-function updateAnimat2() {
-  currentTime = new Date().getTime();
-  if (currentTime - lastTime < FRAME_PERIOD2) {
-    window.requestAnimationFrame(updateAnimat2);
-    return;
-  }
+// function updateAnimat2() {
+//   currentTime = new Date().getTime();
+//   if (currentTime - lastTime < FRAME_PERIOD2) {
+//     window.requestAnimationFrame(updateAnimat2);
+//     return;
+//   }
 
-  if (frameIndex2 >= frameIndexMax2) {
-    let btn = document.getElementById("reloadBtn");
-    btn.style.display = "block";
-    drawRandomImage();
-    return;
-  }
-  frameIndex2++;
+//   if (frameIndex2 >= frameIndexMax2) {
+//     let btn = document.getElementById("reloadBtn");
+//     btn.style.display = "block";
+//     drawRandomImage();
+//     return;
+//   }
+//   frameIndex2++;
 
-  ctx.beginPath();
-  ctx.rect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "rgba(0,0,0,0.3)";
-  ctx.fill();
+//   ctx.beginPath();
+//   ctx.rect(0, 0, canvas.width, canvas.height);
+//   ctx.fillStyle = "rgba(0,0,0,0.3)";
+//   ctx.fill();
 
-  lastTime = currentTime;
-  window.requestAnimationFrame(updateAnimat2);
-}
+//   lastTime = currentTime;
+//   window.requestAnimationFrame(updateAnimat2);
+// }
 
-function drawRandomImage() {
-  let _g = getRndGachapon();
-  // span = document.getElementById("myspan");
-  // txt = document.createTextNode(_g.name);
-  // span.appendChild(txt);
-  let backgroundImage2 = new Image();
-  backgroundImage2.onload = function () {
-    ctx.drawImage(backgroundImage2, x, y, _width, _height);
-  };
-  backgroundImage2.src = "./assets/background2.webp";
+// function drawRandomImage() {
+//   let _g = getRndGachapon();
+//   // span = document.getElementById("myspan");
+//   // txt = document.createTextNode(_g.name);
+//   // span.appendChild(txt);
+//   let backgroundImage2 = new Image();
+//   backgroundImage2.onload = function () {
+//     ctx.drawImage(backgroundImage2, x, y, _width, _height);
+//   };
+//   backgroundImage2.src = "./assets/background2.webp";
 
-  ctx.beginPath();
-  ctx.rect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "rgba(0,0,0,1)";
-  ctx.fill();
+//   ctx.beginPath();
+//   ctx.rect(0, 0, canvas.width, canvas.height);
+//   ctx.fillStyle = "rgba(0,0,0,1)";
+//   ctx.fill();
 
-  let nameImage = new Image();
-  nameImage.onload = function () {
-    ctx.drawImage(
-      nameImage,
-      (screenWidth - _nameWidth) / 2,
-      (screenHeight - _gashponHeight) / 5 + _gashponHeight - 30,
-      _nameWidth,
-      _nameHeight
-    );
-  };
-  nameImage.src = _g.name;
+//   let nameImage = new Image();
+//   nameImage.onload = function () {
+//     ctx.drawImage(
+//       nameImage,
+//       (screenWidth - _nameWidth) / 2,
+//       (screenHeight - _gashponHeight) / 5 + _gashponHeight - 30,
+//       _nameWidth,
+//       _nameHeight
+//     );
+//   };
+//   nameImage.src = _g.name;
 
-  let itemImage = new Image();
-  itemImage.onload = function () {
-    ctx.drawImage(
-      itemImage,
-      (screenWidth - _gashponWidth) / 2,
-      (screenHeight - _gashponHeight) / 5,
-      _gashponWidth,
-      _gashponHeight
-    );
-  };
-  itemImage.src = _g.src;
-}
+//   let itemImage = new Image();
+//   itemImage.onload = function () {
+//     ctx.drawImage(
+//       itemImage,
+//       (screenWidth - _gashponWidth) / 2,
+//       (screenHeight - _gashponHeight) / 5,
+//       _gashponWidth,
+//       _gashponHeight
+//     );
+//   };
+//   itemImage.src = _g.src;
+// }
 
-requestAnimationFrame(updateAnimteHint);
+// requestAnimationFrame(updateAnimteHint);
